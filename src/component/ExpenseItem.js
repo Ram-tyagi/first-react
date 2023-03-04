9b18fb014ff35d17811a28e040062b4c8b3dfe70
Ram-tyagi <ramtyagi04@gmail.com>
2023-03-04 23:15:49 +0530

    first-react-app

---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>Expense Item!</div>
+           <div>
+            <h4>Food Rs100</h4>
+            <h4>Movies Rs200</h4>
+            <h4>Fuel Rs300</h4>
+           </div>
+        </div>
+        
+    )
+  
+}
+export default ExpenseItem;
