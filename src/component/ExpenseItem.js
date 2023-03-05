fae93eebe28a8cdec6f1ac7ca146bbafd2e314d6
Ram-tyagi <ramtyagi04@gmail.com>
2023-03-05 23:11:04 +0530

    Props update

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,13 +1,13 @@
 import './ExpenseItem.css';
-function ExpenseItem() {
-    const expenseLocation='Agra';
+function ExpenseItem(props) {
+    
     return (
         <div className="expense-item">
-            <div>March 4th 2023</div>
+            <div>{props.date.ISOString()}</div>
            <div className="expense-item__description">
-            <h2>Car Insurance</h2>
-            <div>{expenseLocation}</div>
-           <div className="expense-item__price">Rs10000</div>
+            <h2>{props.title}</h2>
+            <div>{props.location}</div>
+           <div className="expense-item__price">{props.amount}</div>
            </div>
         </div>
         
